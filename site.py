d47511d15e44d5f6db4bd0c96789949a6d875f89
alpbay-dev <kutlualpbayir@gmail.com>
2025-06-08 00:37:30 +0300

    Update site.py

--- site.py
+++ site.py
@@ -3,13 +3,11 @@ from app import create_app
 import os
 from flask import Flask
 
-# Tell Flask where to find templates and static files
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 static_dir = os.path.join(os.path.dirname(__file__), 'static')
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
-# Call the factory function to register routes
 create_app(app)
 
 if __name__ == "__main__":
